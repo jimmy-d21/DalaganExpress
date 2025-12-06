014d2231afc1f1cda5a6865684cd493b41034a89
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-12-06 16:09:13 +0800

    Create a checkAvailabilityOfMotor function to check the avalabiliy of the Moror

--- server/controllers/booking.motor.controller.js
+++ server/controllers/booking.motor.controller.js
@@ -1,4 +1,5 @@
-import Booking from "../models/booking.motor.model";
+import Booking from "../models/booking.motor.model.js";
+import Motor from "../models/Motor.js";
 
 // Helper function to check motor availability
 const checkAvailability = async (motorId, pickupDate, returnDate) => {
@@ -18,5 +19,38 @@ const checkAvailability = async (motorId, pickupDate, returnDate) => {
   } catch (error) {
     console.error("Error checking availability:", error);
     return false;
+  }
+};
+
+// API to Check Availability of Motor for given Date and location
+export const checkAvailabilityOfMotor = async (req, res) => {
+  try {
+    const { location, pickupDate, returnDate } = req.body;
+
+    // fetch all available cars for the given location
+    const motor = await Motor.find({
+      location,
+      isAvaliable: true,
+    });
+
+    // Check car availability for the given date range using promise
+    const availableMotorPromises = motor.map(async (motor) => {
+      const isAvaliable = await checkAvailability(
+        motor._id,
+        pickupDate,
+        returnDate
+      );
+      return { ...motor._doc, isAvaliable: isAvaliable };
+    });
+
+    let availableMotor = await Promise.all(availableMotorPromises);
+    availableMotor = availableMotor.filter(
+      (motor) => motor.isAvaliable === true
+    );
+
+    res.json({ success: true, availableMotor });
+  } catch (error) {
+    console.log("Error in checkAvailabilityOfMotor controller");
+    res.json({ success: false, message: error.message });
   }
 };
